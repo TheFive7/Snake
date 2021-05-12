a9763b189240914109436268019716183e9ecf71
TheFive7 <75722116+TheFive7@users.noreply.github.com>
2021-05-12 13:50:16 +0200

    Version 3.2

---
+++ src/game/Serveur.java
@@ -0,0 +1,68 @@
+package game;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+import static game.Game.msg;
+
+public class Serveur {
+    private static final Scanner input = new Scanner(System.in);
+
+    public Serveur() {
+        try {
+            int time = 30000;
+            msg("Temps d'attente maximum: " + time /1000 + "s");
+            ServerSocket sS = new ServerSocket(4242); // Creation du serveur
+            sS.setSoTimeout(time); // Temps d'attente en millis qu'un client se connecte (ici 10 sec)
+            Socket s = sS.accept(); // Le seveur accepte le client
+            msg("Connexion établie avec Succès !");
+
+            PrintWriter out = new PrintWriter(s.getOutputStream()); // Prendre le flux du Socket
+            BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream())); // Récupere les infos
+
+            // Partie envoi des messages
+            String recu = "";
+            String msg = "";
+            while (!recu.equals("stop") && !msg.equals("stop")) {
+                /* ENVOI */
+                System.out.print("-> ");
+                msg = input.nextLine();
+                out.println(msg); // Affiche un message
+                out.flush(); // Envoi le message
+
+                /* RECU */
+                recu = in.readLine();
+                System.out.println(recu); // Affiche l'info récupérée
+            }
+
+
+            sS.close(); // Fermer le serveur
+            s.close(); // Fermer le Socket
+        } catch(IOException e) {
+            msg("Connexion échouée.");
+            System.exit(0);
+        }
+
+    }
+
+    /**
+     * Donne l'adresse IP. (Facultatif)
+     */
+    public static String getIP(){
+        try {
+            InetAddress add = InetAddress.getLocalHost(); // Avoir son adresse IP
+            return String.valueOf(add);
+        } catch (UnknownHostException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
